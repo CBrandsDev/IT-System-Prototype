30c4bfda7a6723258c593d0e81ad9da4d505c0be
CBrandsDev <cbrandao2001@gmail.com>
2023-07-26 20:14:26 -0300

    some changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 const header = document.querySelector('.header')
+const btn = document.querySelectorAll('.btn')
 const login = document.querySelector('.active-login')
 
 
-
+function activeLogin() {
+    btn.addEventListener('click', () => {
+        
+    })
+}
