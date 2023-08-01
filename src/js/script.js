069802860898d9f06b0498585f603456a638e496
CBrandsDev <cbrandao2001@gmail.com>
2023-08-01 17:56:22 -0300

    some changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,15 @@
 const header = document.querySelector(".header");
-const btn = document.querySelectorAll(".btn");
+const main = document.querySelector(".main");
+const btn = document.querySelector(".btn");
 const login = document.querySelector(".active-login");
+const overlay = document.querySelector(".overlay");
 
+function loginEvent() {
+  btn.addEventListener("click", () => {
+    login.classList.toggle("hidden");
+    overlay.style.display = "block";
+  });
+  
+}
 
-btn.addEventListener("click", () => {
-  login.classList.toggle("hidden");
-});
+loginEvent();
