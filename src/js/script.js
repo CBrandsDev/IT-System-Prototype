1c4e44d151489f2d720f119039af721e0afedad0
CBrandsDev <cbrandao2001@gmail.com>
2023-08-01 18:27:42 -0300

    some changes

--- src/js/script.js
+++ src/js/script.js
@@ -3,13 +3,17 @@ const main = document.querySelector(".main");
 const btn = document.querySelector(".btn");
 const login = document.querySelector(".active-login");
 const overlay = document.querySelector(".overlay");
+const exitBtn = document.getElementById("exit");
 
 function loginEvent() {
   btn.addEventListener("click", () => {
     login.classList.toggle("hidden");
     overlay.style.display = "block";
   });
-  
+  exitBtn.addEventListener("click", () => {
+    login.classList.add("hidden");
+    overlay.style.display = "none";
+  });
 }
 
 loginEvent();
