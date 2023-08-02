c81a3dd00e05c090a2bad6bfb314b5aec22afa04
CBrandsDev <cbrandao2001@gmail.com>
2023-08-02 20:38:16 -0300

    some changes

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,7 @@ const btn = document.querySelector(".btn");
 const login = document.querySelector(".active-login");
 const overlay = document.querySelector(".overlay");
 const exitBtn = document.getElementById("exit");
+const container = document.getElementById("container");
 const labels = document.querySelectorAll(".active-login label");
 
 labels.forEach((label) => {
@@ -19,9 +20,11 @@ labels.forEach((label) => {
 function loginEvent() {
   btn.addEventListener("click", () => {
     login.classList.toggle("hidden");
+    container.classList.toggle('hidden')
     overlay.style.display = "block";
   });
   exitBtn.addEventListener("click", () => {
+    container.classList.toggle('hidden')
     login.classList.add("hidden");
     overlay.style.display = "none";
   });
